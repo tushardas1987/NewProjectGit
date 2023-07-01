fd9abd5c56cd33673b47300a6e365b15b338af22
tushar <tushardas1987@gmail.com>
2023-07-02 00:54:07 +0530

    1st

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ain
+    {
+       private ain() { }
+        private static ain _instance;
+        private static object _lock=new object();
+            public static ain instance()
+            {
+                if (_instance == null)
+                {
+                lock (_lock)
+                {
+                    _instance = new ain();
+                }
+                }
+                return _instance;
+            }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
